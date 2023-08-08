0511a07db362bb6655f117389cc753acbd81e5e9
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 00:59:26 +0300

    added url for accessing manage user page

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,6 +10,7 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name='generate-token'),
     path("token/refresh/", TokenRefreshView.as_view(), name='token-refresh'),
+    path("me/", ManageUserView.as_view(), name="manage-user")
 ]
 
 app_name = "user"

96966cacc225f28695b8dc9895b5027077454ecb
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 00:19:41 +0300

    created url for CreateUserView

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
+from django.urls import path
+
+from user.views import CreateUserView
 
 urlpatterns = [
+    path("register", CreateUserView.as_view(), name="create")
 ]

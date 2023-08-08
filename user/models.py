cb0bf43e31f4363c682714cec1a33118d5132d3b
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 00:33:10 +0300

    fixed create user method of custom user manager

--- user/models.py
+++ user/models.py
@@ -18,7 +18,7 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self.create_user(email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)

a5c9a4bb870e1819c581a099235d171f645022ac
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 00:00:53 +0300

    user model registered to admin page

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from user.models import User
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    ordering = ("email", )
--- user/models.py
+++ user/models.py
@@ -20,7 +20,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", False)
         return self.create_user(email, password, **extra_fields)
 
-    def create_super_user(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -33,14 +33,12 @@ class CustomUserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 
-
-
-
 class User(AbstractUser):
+
     username = None
     email = models.EmailField("email address", unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager
+    objects = CustomUserManager()

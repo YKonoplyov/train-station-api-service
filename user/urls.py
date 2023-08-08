67bdf9d8f65b4c83bfec78ec733fcd6c524caeaf
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 00:35:26 +0300

    added user app urls to global urls

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from user.views import CreateUserView
 
 urlpatterns = [
-    path("register", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create")
 ]
+
+app_name = "user"

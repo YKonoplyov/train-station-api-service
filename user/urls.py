c35f22e052ad5d9474d08c58cc92c3da793c0d04
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 00:53:31 +0300

    added authentication through JWT token

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
 from user.views import CreateUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name='generate-token'),
+    path("token/refresh/", TokenRefreshView.as_view(), name='token-refresh'),
 ]
 
 app_name = "user"

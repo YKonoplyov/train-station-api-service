404ccac45a71fb32984209eaddbe526031f9caa0
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 14:10:03 +0300

    registered CrewViewSet to router

--- train_station/urls.py
+++ train_station/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework import routers
 
 from train_station.views import (TrainTypeViewSet, TrainViewSet,
-                                 StationViewSet, RouteViewSet, TripViewSet)
+                                 StationViewSet, RouteViewSet, TripViewSet,
+                                 CrewViewSet)
 
 train_station_router = routers.DefaultRouter()
 train_station_router.register("train_types", TrainTypeViewSet)
@@ -10,6 +11,7 @@ train_station_router.register("trains", TrainViewSet)
 train_station_router.register("stations", StationViewSet)
 train_station_router.register("routs", RouteViewSet)
 train_station_router.register("trips", TripViewSet)
+train_station_router.register("crews", CrewViewSet)
 
 urlpatterns = [path("", include(train_station_router.urls))]
 

47a3cbf39a50427fc1d6773959edb2bc1f1e2a42
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 12:51:03 +0300

    created TrainListSerializer

--- train_station/srializers.py
+++ train_station/srializers.py
@@ -15,3 +15,7 @@ class TrainSerializer(serializers.ModelSerializer):
     class Meta:
         model = Train
         fields = "__all__"
+
+
+class TrainListSerializer(TrainSerializer):
+    train_type = TrainTypeSerializer(many=False, read_only=True)

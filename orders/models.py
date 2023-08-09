f412e4f4211d15bac0b0f4e9f933eeb029358604
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 16:50:32 +0300

    created Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Owner: {self.user.email}. Created at:{str(self.created_at)}"
+
+    class Meta:
+        ordering = ["-created_at"]
+

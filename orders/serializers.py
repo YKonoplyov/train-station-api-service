88848733aec205ba8b9f24765078b96cd545dd55
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 17:31:45 +0300

    created Ticket serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from orders.models import Ticket
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["movie_session"].cinema_hall,
+            ValidationError
+        )
+        return data
+
+    class Meta:
+        model = Ticket
+        fields = "__all__"

1c7e49a94b7793eb30040be2725c16c3eecf23f8
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 20:41:39 +0300

    added pagination to Order viewset

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -5,10 +6,18 @@ from orders.models import Order
 from orders.serializers import OrderSerializer
 
 
+class OrderPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
     permission_classes = [IsAuthenticated]
+    pagination_class = OrderPagination
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)

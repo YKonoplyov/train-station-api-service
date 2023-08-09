676f3b80917a6f6aabae4ef9fd1731f14fdbfa48
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 16:53:40 +0300

    orders models registered to admin page

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -16,7 +16,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
+
+    "orders",
+    "train_station",
     "user",
 ]
 

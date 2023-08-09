7bed7cc16b300ded5b084f646b467bd86cb00aa6
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 12:18:15 +0300

    added train_station urls to global urls

--- train_station_service/urls.py
+++ train_station_service/urls.py
@@ -4,4 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path(
+        "api/train_station/",
+        include("train_station.urls", namespace="train_station")
+    )
 ]

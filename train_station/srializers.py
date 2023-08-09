dfbddeba45f4a9859f69d075cbc2afe5ca01b126
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 11:57:12 +0300

    created serializer for train model

--- train_station/srializers.py
+++ train_station/srializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from train_station.models import TrainType
+from train_station.models import TrainType, Train
 
 
 class TrainTypeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TrainType
+        fields = "__all__"
+
+
+class TrainSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Train
         fields = "__all__"

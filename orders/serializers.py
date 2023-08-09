a2847fcc7a16ea4a90cd66198b2d4217cbdfefcc
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-09 17:39:33 +0300

    created Order serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,8 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from orders.models import Ticket
+from orders.models import Ticket, Order
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -18,3 +19,19 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer
+
+    class Meta:
+        model = Order
+        fields = ("id", "created_at", "tickets", )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order

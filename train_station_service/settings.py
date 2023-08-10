cbb888c4550a24373bde8f050f022828b48281b7
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-10 18:14:27 +0300

    changed sittings for default db

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -1,5 +1,9 @@
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,8 +65,11 @@ WSGI_APPLICATION = "train_station_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
